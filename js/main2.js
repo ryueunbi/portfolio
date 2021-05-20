989c3f7227206e34e881e506aa91fb959509490a
alswjd1125 <alslal1125@naver.com>
2021-05-20 14:24:26 +0900

    21-05-20_PM0224

--- js/main2.js
+++ js/main2.js
@@ -31,34 +31,3 @@
             clearInterval(tyInt);}
         } 
     };
-
-// tiny slider
-    document.addEventListener("DOMContentLoaded", function(e){
-        const contents = document.querySelector("section#contents");
-
-        const slider = document.querySelector("div.slider");
-        const imgSlider = tns({
-            container: slider,
-            nav: false,
-            tems: 1,
-            mouseDrag: true,
-            // loop: true,
-            // autoplay: true,
-            // autoplayHoverPause: true,
-            // autoplayTimeout: 2500,
-            controls: false
-        });
-
-        for (let i = 0; i < imgSliderPrev.length; i++){
-            imgSliderPrev[i].onclick = (() => {
-                imgSlider.goTo("prev");
-            });
-        }
-
-        for (let i = 0; i < imgSliderNext.length; i++){
-            imgSliderNext[i].onclick = (() => {
-                imgSlider.goTo("next");
-            });
-        }
-
-    });
