85cdf45b28ea2f08dab8c00f4e0a24afdc753fdc
alswjd1125 <alslal1125@naver.com>
2021-05-21 17:51:29 +0900

    2021-05-21PM6

--- js/main.js
+++ js/main.js
@@ -33,35 +33,90 @@
     };
 
 // tiny slider
-document.addEventListener("DOMContentLoaded", function(e){
-    const contents = document.querySelector("section#contents");
+    document.addEventListener("DOMContentLoaded", function(e){
+        const contents = document.querySelector("section#contents");
 
-    const slider = document.querySelector("div.slider");
-    const imgSlider = tns({
-        container: slider,
-        nav: false,
-        tems: 1,
-        mouseDrag: true,
-        // loop: true,
-        // autoplay: true,
-        // autoplayHoverPause: true,
-        // autoplayTimeout: 2500,
-        controls: false
-    });
-
-    const imgSliderPrev = document.querySelectorAll("ul.sliderControl button.prev");
-    const imgSliderNext = document.querySelectorAll("ul.sliderControl button.next");
-
-    for (let i = 0; i < imgSliderPrev.length; i++){
-        imgSliderPrev[i].onclick = (() => {
-            imgSlider.goTo("prev");
+        const slider = document.querySelector("div.slider");
+        const imgSlider = tns({
+            container: slider,
+            nav: false,
+            tems: 1,
+            mouseDrag: true,
+            // loop: true,
+            // autoplay: true,
+            // autoplayHoverPause: true,
+            // autoplayTimeout: 2500,
+            controls: false
         });
+
+        const imgSliderPrev = document.querySelectorAll("ul.sliderControl button.prev");
+        const imgSliderNext = document.querySelectorAll("ul.sliderControl button.next");
+
+        for (let i = 0; i < imgSliderPrev.length; i++){
+            imgSliderPrev[i].onclick = (() => {
+                imgSlider.goTo("prev");
+            });
+        }
+
+        for (let i = 0; i < imgSliderNext.length; i++){
+            imgSliderNext[i].onclick = (() => {
+                imgSlider.goTo("next");
+            });
+        }
+
+// cursor custom
+    var mouse = document.querySelector(".cursor");
+    var links = document.querySelectorAll("a");
+    var bounce = "bounce";
+    var inactive = "inactive"; // function that make the circle follows the mouse pointer
+
+    function moveMouse(e) {
+    var x = e.clientX;
+    var y = e.clientY;
+    mouse.style.transform = "translate("
+        .concat(x - 15, "px, ")
+        .concat(y - 15, "px)");
+    } // function that turn on/off the animation
+
+    function disableAnimation() {
+    var hasBounceClass = mouse.classList.contains(bounce);
+
+    if (hasBounceClass) {
+        mouse.classList.add(inactive);
+        mouse.classList.remove(bounce);
+    } else {
+        mouse.classList.add(bounce);
+        mouse.classList.remove(inactive);
+    }
+    } // check for when the mouse is being moving
+
+    document.addEventListener("mousemove", moveMouse); // check wether the user hover/leave a link
+
+    for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("mouseover", disableAnimation);
+    links[i].addEventListener("mouseleave", disableAnimation);
     }
 
-    for (let i = 0; i < imgSliderNext.length; i++){
-        imgSliderNext[i].onclick = (() => {
-            imgSlider.goTo("next");
-        });
-    }
+// menu
+    $('#toggle').click(function(){
+        $('#toggle>ul').toggleClass('animate');
+        $('#page').toggleClass('overlay');
+    })
+    
+    $('.aboutbtn').click(function(){
+        $('.aboutbtn').css('display', 'none')
+        $('.closebtn').css('display', 'block')
+        $('body').css('overflow', 'hidden')
+    })
+    
+    $('.closebtn').click(function(){
+        $('.aboutbtn').css('display', 'block')
+        $('.closebtn').css('display', 'none')
+        $('body').css('overflow', 'scroll')
+    })
+    
+    $('#overlay>nav>ul>li>a').click(function(){
+        $('body').css('overflow', 'scroll')
+    })
 
 });
