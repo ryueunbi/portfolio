84ac189bf84dc042f34202e4dcc00ab4a95bc9e0
ryueunbi <ryueunbi19@gmail.com>
2021-05-25 07:09:28 +0900

    v0525-2

--- js/main.js
+++ js/main.js
@@ -62,7 +62,50 @@ document.addEventListener("DOMContentLoaded", function(e) {
       imgSliderNext[i].onclick = (() => {
         imgSlider.goTo("next");
     });
+  }
+  
+  const slidePageNumCurrent = document.querySelectorAll("p.num > span.current");
+  const slidePageNumTotal = document.querySelectorAll("p.num > span.total");
+  imgSlider.events.on("transitionEnd", function(){
+    slidePageNumCurrent.innerText = imgSlider.getInfo().slideCount;
+    slidePageNumTotal.innerText = imgSlider.getInfo().index;
+  });
+
+  const ideafestival = scrollMonitor.create(document.querySelectorAll("div.title.ideafestival"));
+  const audiotechnica = scrollMonitor.create(document.querySelectorAll("div.title.at"));
+  const shopick = scrollMonitor.create(document.querySelectorAll("div.title.shopick"));
+  const brothumb = scrollMonitor.create(document.querySelectorAll("div.title.brothumb"));
+
+  ideafestival.enterViewport(function() {
+    for(let i = 0; i < 4; i++) {
+      document.querySelector("div.slider").children[i].querySelector("img").src = "./images/img_ideafestival0" + (i+1) + ".jpg"
+      // contents.style.background = "url('./common/images/bg_ideafestival.jpg') fixed no-repeat";
     }
+  });
+
+  audiotechnica.enterViewport(function() {
+    for(let i = 0; i < 4; i++) {
+      document.querySelector("div.slider").children[i].querySelector("img").src = "./images/img_at0" + (i+1) + ".jpg"
+      // contents.style.background = "url('./common/images/bg_at.jpg') fixed no-repeat";
+    }
+  });
+
+  shopick.enterViewport(function() {
+    for(let i = 0; i < 4; i++) {
+      document.querySelector("div.slider").children[i].querySelector("img").src = "./images/img_shopick0" + (i+1) + ".jpg"
+      // contents.style.background = "url('./common/images/bg_shopick.jpg') fixed no-repeat";
+    }
+  });
+
+  brothumb.enterViewport(function() {
+    for(let i = 0; i < 4; i++) {
+      document.querySelector("div.slider").children[i].querySelector("img").src = "./images/img_brothumb0" + (i+1) + ".jpg"
+      // contents.style.background = "url('./common/images/bg_brothumb.jpg') fixed no-repeat";
+    }
+  });
+
+  window.addEventListener("scroll", function(e) {
+  })
   
   
 
